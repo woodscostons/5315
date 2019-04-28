4958ffb41db3f198ccabe3843bcb5ed797aad786
spencer <woodscos@seattleu.edu>
2019-04-28 10:34:58 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,12 +1,12 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "name": "Bob Runn",
+      "designation": "Account Manager",
+      "salary": "$130,350",
+      "joining_date": "2001/05/15",
+      "office": "Seattle",
+      "extension": "8122"
     },
     {
       "name": "Brielle Williamson",
